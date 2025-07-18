a3feec67814c704b2a6f095b93cc70bb6e7b10af
Gabriel Andrade <gabriel_anme@hotmail.com>
2025-07-18 17:08:35 -0300

    mentoria e curso

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("Descrição Curso Javascript");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição Mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+    }
+}
